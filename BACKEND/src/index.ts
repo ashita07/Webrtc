2dc26b387cc887bd05e8444ae0f08d504e64c763
Ashita <ashitam2407@gmail.com>
2025-01-04 21:09:58 +0530

    made a ping pong web server

--- BACKEND/src/index.ts
+++ BACKEND/src/index.ts
@@ -1,1 +1,13 @@
-console.log("hi");
+import { WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on("connection", function (socket) {
+  console.log("user connected");
+
+  socket.on("message", (e) => {
+    if (e.toString() === "ping") {
+      socket.send("pong");
+    }
+  });
+});
